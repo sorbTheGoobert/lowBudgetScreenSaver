52dbd734c20093dfa3ceaf6f2eb93ba694500d7e
sorbOnWSL <reeldob34@gmail.com>
2024-10-11 14:19:58 +0800

    bruh i needed to fix this code (insert praying emoji (bro i dont even use tiktok)) I still havent added few stuffs yet but this is fine for its state

--- script.js
+++ script.js
@@ -1,197 +1,163 @@
-var box1 = document.getElementById("box1");
-var box2 = document.getElementById("box2");
-var box3 = document.getElementById("box3");
-var box4 = document.getElementById("box4");
-var box5 = document.getElementById("box5");
-var box6 = document.getElementById("box6");
-var box7 = document.getElementById("box7");
-var box8 = document.getElementById("box8");
-var box9 = document.getElementById("box9");
-var box10 = document.getElementById("box10");
-/*
-var box11 = document.getElementById("box11");
-var box12 = document.getElementById("box12");
-var box13 = document.getElementById("box13");
-var box14 = document.getElementById("box14");
-var box15 = document.getElementById("box15");
-var box16 = document.getElementById("box16");
-var box17 = document.getElementById("box17");
-var box18 = document.getElementById("box18");
-var box19 = document.getElementById("box19");
-var box20 = document.getElementById("box20");
-var box21 = document.getElementById("box21");
-var box22 = document.getElementById("box22");
-var box23 = document.getElementById("box23");
-var box24 = document.getElementById("box24");
-var box25 = document.getElementById("box25");
-*/
-var constX = Math.floor(Math.random() * 2);
-var constY = Math.floor(Math.random() * 2);
-var box = [/*box25,box24,box23,box22,box21,box20,box19,box18,box17,box16,box15,box14,box13,box12,box11,*/box10, box9, box8, box7, box6, box5, box4, box3, box2, box1];
-var savedColor;
-var tokenx = [constX, constX, constX, constX, constX, constX, constX, constX, constX, constX];
-var tokeny = [constY, constY, constY, constY, constY, constY, constY, constY, constY, constY];
-var r, g, b;
-var devHeight = document.body.offsetHeight - 100;
-var devWidth = document.body.offsetWidth - 100;
-var ranX = Math.floor(Math.random() * (devWidth - 100)) + 100;
-var ranY = Math.floor(Math.random() * (devHeight - 100)) + 100;
-var loopId;
-var text1 = document.getElementById("text1");
-var text2 = document.getElementById("text2");
-var speed = 1;
+/**
+ * * Here is the quote
+ * You are well lucky
+ * Now seek the Gargantua
+ */
 
-function randomColor() {
-    return Math.floor(Math.random() * 256);
+function randomizeColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return [r, g, b];
 }
 
-//change 10 to 25  for chaos also activate every comment for 25 afterimages
+class Bouncer {
+  constructor(length, speed, size, initPos) {
+    this.SPEED = speed;
+    this.BOX_SIZE = size;
 
-r = randomColor();
-g = randomColor();
-b = randomColor();
-
-
-for (i = 0; i < 10; i++) {
-    box[i].style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-    console.log(box[i].style.backgroundColor);
+    this.head = new Head(
+      initPos,
+      randomizeColor(),
+      [this.SPEED, this.SPEED],
+      this.BOX_SIZE
+    );
+    this.box = [];
+    const stepSize = 0.9 / (length - 1);
+    let opacity = 1 - stepSize;
+    for (let i = 0; i < length - 1; i++) {
+      this.box.push(
+        new Box(
+          initPos,
+          randomizeColor(),
+          [this.SPEED, this.SPEED],
+          this.BOX_SIZE,
+          opacity
+        )
+      );
+      opacity -= stepSize;
+    }
+  }
+  update = () => {
+    const width = document.body.offsetWidth;
+    const height = document.body.offsetHeight;
+    for (let i = this.box.length - 1; i > 0; i--) {
+      this.box[i].update(this.box[i - 1].pos, this.box[i - 1].color);
+    }
+    this.box[0].update(this.head.pos, this.head.color);
+    this.head.update(width, height);
+  };
 }
 
-function move(i, j) {
-    var x = box[i].offsetLeft;
-    var y = box[i].offsetTop;
-    if (tokenx[i] % 2 == 0) {
-        x += j;
-        if (x > devWidth) {
-            x -= j;
-            tokenx[i]++;
-            if (i == 9) {
-                box[i].style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`
-                savedColor = box[i].style.backgroundColor;
-            }
-            else {
-                box[i].style.backgroundColor = savedColor;
-            }
-        }
-    }
-    else {
-        x -= j;
-        if (x < 0) {
-            x += j;
-            tokenx[i]++;
-            if (i == 9) {
-                box[i].style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`
-                savedColor = box[i].style.backgroundColor;
-            }
-            else {
-                box[i].style.backgroundColor = savedColor;
-            }
-        }
-    }
-    if (tokeny[i] % 2 == 0) {
-        y += j;
-        if (y > devHeight) {
-            y -= j;
-            tokeny[i]++;
-            if (i == 9) {
-                box[i].style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`
-                savedColor = box[i].style.backgroundColor;
-            }
-            else {
-                box[i].style.backgroundColor = savedColor;
-            }
-        }
-    }
-    else {
-        y -= j;
-        if (y < 0) {
-            y += j;
-            tokeny[i]++;
-            if (i == 9) {
-                box[i].style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`
-                savedColor = box[i].style.backgroundColor;
-            }
-            else {
-                box[i].style.backgroundColor = savedColor;
-            }
-        }
-    }
-    box[i].style.left = `${x}px`;
-    box[i].style.top = `${y}px`;
+class Head {
+  constructor(pos, color, speed, BOX_SIZE) {
+    this.pos = [...pos];
+    this.color = [...color];
+    this.speed = [...speed];
+    this.size = BOX_SIZE;
 
-    if ((x == 0 && y == 0) || (x == devWidth && y == 0) || (x == 0 && y == devHeight) || (x == devWidth && y == devHeight)) {
-        monologue();
+    this.div = document.createElement("div");
+    this.div.style.backgroundColor = `rgb(${this.color[0]}, ${this.color[1]}, ${this.color[2]})`;
+    this.div.style.width = `${this.size}px`;
+    this.div.style.height = `${this.size}px`;
+    this.div.style.left = `${this.pos[0]}px`;
+    this.div.style.top = `${this.pos[1]}px`;
+    document.body.appendChild(this.div);
+  }
+  update = (width, height) => {
+    this.pos[0] += this.speed[0];
+    if (this.pos[0] < 0) {
+      this.pos[0] -= this.speed[0];
+      this.speed[0] *= -1;
+      this.color = [...randomizeColor()];
+    }
+    if (this.pos[0] + this.size > width) {
+      this.pos[0] -= this.speed[0];
+      this.speed[0] *= -1;
+      this.color = [...randomizeColor()];
     }
 
-}
-
-function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
-
-async function monologue() {
-    for (let q = 0; q < 10; q++) {
-        box[q].style.backgroundColor = "rgb(0, 0, 0)";
+    this.pos[1] += this.speed[1];
+    if (this.pos[1] < 0) {
+      this.pos[1] -= this.speed[1];
+      this.speed[1] *= -1;
+      this.color = [...randomizeColor()];
     }
-    document.body.style.backgroundColor = "rgb(255, 255, 255)";
-    clearInterval(loopId);
-    await sleep(3000)
-    await blockFadeAway()
-}
-
-async function blockFadeAway() {
-    for (let i = 0; i < 10; i++) {
-        box[i].classList.add("fade");
-        box[i].style.opacity = 0;
-    }
-    await sleep(3000);
-    text1Appear();
-}
-
-async function text1Appear() {
-    text1.style.opacity = 1;
-    await sleep(3000);
-    text2Appear();
-}
-
-async function text2Appear() {
-    text2.style.opacity = 1;
-    await sleep(3000);
-    nothing();
-}
-
-async function nothing() {
-    text1.style.opacity = 0;
-    text2.style.opacity = 0;
-    await sleep(3000);
-    youHavePassedTheTest();
-}
-
-async function youHavePassedTheTest(){
-    location.reload();
-}
-
-function change() {
-    devHeight = document.body.offsetHeight - 100;
-    devWidth = document.body.offsetWidth - 100;
-    for (let i = 0; i < 10; i++) {
-        move(i, speed);
-    }
-}
-
-function startup() {
-
-    for (let i = 0; i <= 9; i++) {
-        box[i].style.left = ranX + "px";
-        box[i].style.top = ranY + "px";
+    if (this.pos[1] + this.size > height) {
+      this.pos[1] -= this.speed[1];
+      this.speed[1] *= -1;
+      this.color = [...randomizeColor()];
     }
 
-
-    for (let i = 0; i <= 9; i++) {
-        for (let j = 0; j < i; j++) {
-            //change these 10 to get some weird ares (from calamatiy ofcourse) chains (still staying family friendly)
-            move(i, 10);
-        }
-    }
-
-    loopId = setInterval(change, 1);
-
+    this.div.style.left = `${this.pos[0]}px`;
+    this.div.style.top = `${this.pos[1]}px`;
+    this.div.style.backgroundColor = `rgb(${this.color[0]}, ${this.color[1]}, ${this.color[2]})`;
+  };
 }
+
+class Box {
+  constructor(pos, color, speed, BOX_SIZE, opac) {
+    this.pos = [...pos];
+    this.color = [...color];
+    this.speed = [...speed];
+    this.size = BOX_SIZE;
+    this.opac = opac;
+
+    this.div = document.createElement("div");
+    this.div.style.backgroundColor = `rgba(${this.color[0]}, ${this.color[1]}, ${this.color[2]}, ${this.opac})`;
+    this.div.style.width = `${this.size}px`;
+    this.div.style.height = `${this.size}px`;
+    this.div.style.left = `${this.pos[0]}px`;
+    this.div.style.top = `${this.pos[1]}px`;
+    document.body.appendChild(this.div);
+  }
+  update = (pos, color) => {
+    this.pos = [...pos];
+    this.color = [...color];
+    this.div.style.left = `${this.pos[0]}px`;
+    this.div.style.top = `${this.pos[1]}px`;
+    this.div.style.backgroundColor = `rgba(${this.color[0]}, ${this.color[1]}, ${this.color[2]}, ${this.opac})`;
+  };
+}
+
+const Bouncers = [];
+
+function addBouncers(settings) {
+  for (let b = 0; b < settings.length; b++) {
+    const temp = settings[b];
+    console.log(temp);
+    Bouncers.push(new Bouncer(...temp));
+  }
+}
+function updateBouncers() {
+  Bouncers.forEach((element) => {
+    element.update();
+  });
+}
+function init(settings) {
+  addBouncers(settings);
+  setInterval(updateBouncers, 1000 / 60);
+}
+function randomLocationOnScreen(size) {
+  return [
+    Math.floor(Math.random() * (document.body.offsetWidth - size)),
+    Math.floor(Math.random() * (document.body.offsetHeight - size)),
+  ];
+}
+window.onload = init([
+  [10, 5, 50, randomLocationOnScreen(50)],
+  [10, 5, 50, randomLocationOnScreen(50)],
+  [10, 5, 50, randomLocationOnScreen(50)],
+  [10, 5, 50, randomLocationOnScreen(50)],
+  [10, 5, 50, randomLocationOnScreen(50)],
+  [10, 5, 50, randomLocationOnScreen(50)],
+  [10, 5, 50, randomLocationOnScreen(50)],
+  [10, 5, 50, randomLocationOnScreen(50)],
+  [10, 5, 50, randomLocationOnScreen(50)],
+  [10, 5, 50, randomLocationOnScreen(50)],
+  [10, 5, 50, randomLocationOnScreen(50)],
+  [10, 5, 50, randomLocationOnScreen(50)],
+  [10, 5, 50, randomLocationOnScreen(50)],
+  [10, 5, 50, randomLocationOnScreen(50)],
+  [10, 5, 50, randomLocationOnScreen(50)],
+]);
