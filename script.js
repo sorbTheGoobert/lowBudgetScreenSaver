fce148e2ddc4a017bcc899694221e8150d03cc75
= <=>
2024-10-16 18:03:29 +0800

    ldkfldsnfls skibidi sigma (insert the cringiest chain of emojis)

--- script.js
+++ script.js
@@ -21,8 +21,8 @@ class Bouncer {
     this.BOX_SIZE = size;
     this.container = document.createElement("div");
     this.container.style.position = "absolute";
-    this.container.style.left = "0px"
-    this.container.style.top = "0px"
+    this.container.style.left = "0px";
+    this.container.style.top = "0px";
     document.body.appendChild(this.container);
 
     this.head = new Head(
@@ -34,30 +34,35 @@ class Bouncer {
       this.container
     );
     this.box = [];
-    const stepSize = 0.9 / (length - 1);
-    let opacity = 1 - stepSize;
+    const opacStep = 0.9 / (length - 1);
+    let opacity = 1 - opacStep;
+    const sizeStep = this.BOX_SIZE / (length - 1);
+    let curSize = this.BOX_SIZE;
     for (let i = 0; i < length - 1; i++) {
       this.box.push(
         new Box(
           initPos,
           randomizeColor(),
           [this.SPEED, this.SPEED],
-          this.BOX_SIZE,
+          curSize,
           opacity,
           length - 1 - i,
           this.container
         )
       );
-      opacity -= stepSize;
+      opacity -= opacStep;
+      curSize -= sizeStep;
     }
   }
   update = () => {
     const width = document.body.offsetWidth;
     const height = document.body.offsetHeight;
-    for (let i = this.box.length - 1; i > 0; i--) {
-      this.box[i].update(this.box[i - 1].pos, this.box[i - 1].color);
+    if (this.box.length > 0) {
+      for (let i = this.box.length - 1; i > 0; i--) {
+        this.box[i].update(this.box[i - 1].pos, this.box[i - 1].color);
+      }
+      this.box[0].update(this.head.pos, this.head.color);
     }
-    this.box[0].update(this.head.pos, this.head.color);
     this.head.update(width, height);
   };
 }
@@ -73,8 +78,8 @@ class Head {
     this.div.style.backgroundColor = `rgb(${this.color[0]}, ${this.color[1]}, ${this.color[2]})`;
     this.div.style.width = `${this.size}px`;
     this.div.style.height = `${this.size}px`;
-    this.div.style.left = `${this.pos[0]}px`;
-    this.div.style.top = `${this.pos[1]}px`;
+    this.div.style.left = `${this.pos[0] - this.size / 2}px`;
+    this.div.style.top = `${this.pos[1] - this.size / 2}px`;
     this.div.style.zIndex = index;
     parent.appendChild(this.div);
 
@@ -82,14 +87,14 @@ class Head {
   }
   update = (width, height) => {
     this.pos[0] += this.speed[0];
-    if (this.pos[0] < 0 && !this.stuck[0]) {
+    if (this.pos[0] - this.size / 2 < 0 && !this.stuck[0]) {
       this.pos[0] -= this.speed[0];
       this.speed[0] = this.speed[0] < 0 ? -this.speed[0] : this.speed[0];
 
       // Inconsistent but could try
       // this.speed[0] *= -1;
       this.color = [...randomizeColor()];
-    } else if (this.pos[0] + this.size > width && !this.stuck[0]) {
+    } else if (this.pos[0] + this.size / 2 > width && !this.stuck[0]) {
       this.pos[0] -= this.speed[0];
       this.speed[0] = this.speed[0] > 0 ? -this.speed[0] : this.speed[0];
 
@@ -99,14 +104,14 @@ class Head {
     }
 
     this.pos[1] += this.speed[1];
-    if (this.pos[1] < 0 && !this.stuck[1]) {
+    if (this.pos[1] - this.size / 2 < 0 && !this.stuck[1]) {
       this.pos[1] -= this.speed[1];
       this.speed[1] = this.speed[1] < 0 ? -this.speed[1] : this.speed[1];
       this.color = [...randomizeColor()];
 
       // Inconsistent but could try
       // this.speed[1] *= -1;
-    } else if (this.pos[1] + this.size > height && !this.stuck[1]) {
+    } else if (this.pos[1] + this.size / 2 > height && !this.stuck[1]) {
       this.pos[1] -= this.speed[1];
       this.speed[1] = this.speed[1] > 0 ? -this.speed[1] : this.speed[1];
       this.color = [...randomizeColor()];
@@ -115,12 +120,20 @@ class Head {
       // this.speed[1] *= -1;
     }
 
-    this.div.style.left = `${this.pos[0]}px`;
-    this.div.style.top = `${this.pos[1]}px`;
+    this.div.style.left = `${this.pos[0] - this.size / 2}px`;
+    this.div.style.top = `${this.pos[1] - this.size / 2}px`;
+    // this.div.style.left = `${this.pos[0]}px`;
+    // this.div.style.top = `${this.pos[1]}px`;
     this.div.style.backgroundColor = `rgb(${this.color[0]}, ${this.color[1]}, ${this.color[2]})`;
 
-    this.stuck[0] = xor(this.pos[0] < 0, this.pos[0] + this.size > width);
-    this.stuck[1] = xor(this.pos[1] < 0, this.pos[1] + this.size > height);
+    this.stuck[0] = xor(
+      this.pos[0] - this.size / 2 < 0,
+      this.pos[0] + this.size / 2 > width
+    );
+    this.stuck[1] = xor(
+      this.pos[1] - this.size / 2 < 0,
+      this.pos[1] + this.size / 2 > height
+    );
   };
 }
 
@@ -136,16 +149,16 @@ class Box {
     this.div.style.backgroundColor = `rgba(${this.color[0]}, ${this.color[1]}, ${this.color[2]}, ${this.opac})`;
     this.div.style.width = `${this.size}px`;
     this.div.style.height = `${this.size}px`;
-    this.div.style.left = `${this.pos[0]}px`;
-    this.div.style.top = `${this.pos[1]}px`;
+    this.div.style.left = `${this.pos[0] - this.size / 2}px`;
+    this.div.style.top = `${this.pos[1] - this.size / 2}px`;
     this.div.style.zIndex = index;
     parent.appendChild(this.div);
   }
   update = (pos, color) => {
     this.pos = [...pos];
     this.color = [...color];
-    this.div.style.left = `${this.pos[0]}px`;
-    this.div.style.top = `${this.pos[1]}px`;
+    this.div.style.left = `${this.pos[0] - this.size / 2}px`;
+    this.div.style.top = `${this.pos[1] - this.size / 2}px`;
     this.div.style.backgroundColor = `rgba(${this.color[0]}, ${this.color[1]}, ${this.color[2]}, ${this.opac})`;
   };
 }
@@ -155,7 +168,6 @@ const Bouncers = [];
 function addBouncers(settings) {
   for (let b = 0; b < settings.length; b++) {
     const temp = settings[b];
-    console.log(temp);
     Bouncers.push(new Bouncer(...temp));
   }
 }
@@ -176,8 +188,8 @@ function randomLocationOnScreen(size) {
 }
 window.onload = init([
   // [2, 10, 500, randomLocationOnScreen(500)],
-  [10, 10, 50, randomLocationOnScreen(50)],
-  [10, 10, 50, randomLocationOnScreen(50)],
-  [10, 10, 50, randomLocationOnScreen(50)],
-  [10, 10, 50, randomLocationOnScreen(50)],
+  [2, 10, 100, randomLocationOnScreen(50)],
+  [2, 10, 100, randomLocationOnScreen(50)],
+  [2, 10, 100, randomLocationOnScreen(50)],
+  [2, 10, 100, randomLocationOnScreen(50)],
 ]);
