3b0dda1f188b6b01e26d461c4e8fa6377b1f9624
= <=>
2024-11-06 18:38:30 +0800

    Ok gotta fix this offset issue

--- script.js
+++ script.js
@@ -11,43 +11,66 @@ function randomizeColor() {
   return [r, g, b];
 }
 
+function randomLocationOnScreen(w, h, s) {
+  return [
+    Math.floor(Math.random() * (w - s)),
+    Math.floor(Math.random() * (h - s)),
+  ];
+}
+
 function xor(x, y) {
   return !(x && y) && (x || y);
 }
 
+class BouncerContainer {
+  constructor(props) {
+    this.container = document.createElement("div");
+    this.container.style.position = "relative";
+    this.container.style.width = `${props.w}px`;
+    this.container.style.height = `${props.h}px`;
+    props.p.appendChild(this.container);
+    this.Bouncers = [];
+    for (let B = 0; B < props.b.length; B++) {
+      this.Bouncers.push(new Bouncer({ ...props.b[B], p: this.container }));
+    }
+  }
+  init = () => {
+    setInterval(this.update, 1000 / 10);
+  }
+  update = () => {
+    this.Bouncers.forEach((element) => {
+      element.update();
+    });
+  }
+}
+
 class Bouncer {
-  constructor(length, speed, size, initPos) {
-    this.SPEED = speed;
-    this.BOX_SIZE = size;
+  constructor(props) {
+
+
+    this.SPEED = props.v;
+    this.BOX_SIZE = props.s;
+    this.offset = props.o
     this.container = document.createElement("div");
     this.container.style.position = "absolute";
     this.container.style.left = "0px";
     this.container.style.top = "0px";
-    document.body.appendChild(this.container);
+    this.parent = props.p;
+    this.parent.appendChild(this.container);
 
     this.head = new Head(
-      initPos,
-      randomizeColor(),
-      [this.SPEED, this.SPEED],
-      this.BOX_SIZE,
-      length,
-      this.container
+      { i: props.i, c: randomizeColor(), v: [this.SPEED, this.SPEED], s: this.BOX_SIZE, z: props.l, p: this.container },
     );
+
     this.box = [];
-    const opacStep = 0.9 / (length - 1);
+    const opacStep = 0.9 / (props.l);
     let opacity = 1 - opacStep;
-    const sizeStep = this.BOX_SIZE / (length - 1);
+    const sizeStep = this.BOX_SIZE / (props.l);
     let curSize = this.BOX_SIZE;
-    for (let i = 0; i < length - 1; i++) {
+    for (let i = 0; i < props.l; i++) {
       this.box.push(
         new Box(
-          initPos,
-          randomizeColor(),
-          [this.SPEED, this.SPEED],
-          curSize,
-          opacity,
-          length - 1 - i,
-          this.container
+          { i: props.i, c: randomizeColor(), s: curSize, o: opacity, f: this.offset, z: props.l - i, p: this.container }
         )
       );
       opacity -= opacStep;
@@ -55,8 +78,8 @@ class Bouncer {
     }
   }
   update = () => {
-    const width = document.body.offsetWidth;
-    const height = document.body.offsetHeight;
+    const width = this.parent.offsetWidth;
+    const height = this.parent.offsetHeight;
     if (this.box.length > 0) {
       for (let i = this.box.length - 1; i > 0; i--) {
         this.box[i].update(this.box[i - 1].pos, this.box[i - 1].color);
@@ -68,11 +91,12 @@ class Bouncer {
 }
 
 class Head {
-  constructor(pos, color, speed, BOX_SIZE, index, parent) {
-    this.pos = [...pos];
-    this.color = [...color];
-    this.speed = [...speed];
-    this.size = BOX_SIZE;
+  constructor(props) {
+
+    this.pos = [...props.i];
+    this.color = [...props.c];
+    this.speed = [...props.v];
+    this.size = props.s;
 
     this.div = document.createElement("div");
     this.div.style.backgroundColor = `rgb(${this.color[0]}, ${this.color[1]}, ${this.color[2]})`;
@@ -80,8 +104,8 @@ class Head {
     this.div.style.height = `${this.size}px`;
     this.div.style.left = `${this.pos[0] - this.size / 2}px`;
     this.div.style.top = `${this.pos[1] - this.size / 2}px`;
-    this.div.style.zIndex = index;
-    parent.appendChild(this.div);
+    this.div.style.zIndex = props.z;
+    props.p.appendChild(this.div);
 
     this.stuck = [false, false];
   }
@@ -122,8 +146,6 @@ class Head {
 
     this.div.style.left = `${this.pos[0] - this.size / 2}px`;
     this.div.style.top = `${this.pos[1] - this.size / 2}px`;
-    // this.div.style.left = `${this.pos[0]}px`;
-    // this.div.style.top = `${this.pos[1]}px`;
     this.div.style.backgroundColor = `rgb(${this.color[0]}, ${this.color[1]}, ${this.color[2]})`;
 
     this.stuck[0] = xor(
@@ -138,12 +160,13 @@ class Head {
 }
 
 class Box {
-  constructor(pos, color, speed, BOX_SIZE, opac, index, parent) {
-    this.pos = [...pos];
-    this.color = [...color];
-    this.speed = [...speed];
-    this.size = BOX_SIZE;
-    this.opac = opac;
+  constructor(props) {
+
+    this.pos = [...props.i];
+    this.color = [...props.c];
+    this.size = props.s;
+    this.opac = props.o;
+    this.offset = props.f;
 
     this.div = document.createElement("div");
     this.div.style.backgroundColor = `rgba(${this.color[0]}, ${this.color[1]}, ${this.color[2]}, ${this.opac})`;
@@ -151,11 +174,33 @@ class Box {
     this.div.style.height = `${this.size}px`;
     this.div.style.left = `${this.pos[0] - this.size / 2}px`;
     this.div.style.top = `${this.pos[1] - this.size / 2}px`;
-    this.div.style.zIndex = index;
-    parent.appendChild(this.div);
+    this.div.style.zIndex = props.z;
+    props.p.appendChild(this.div);
   }
   update = (pos, color) => {
+    const delta = [
+      this.pos[0] - pos[0],
+      this.pos[1] - pos[1]
+    ];
+    // if (
+    //   Math.abs(delta[0]) > this.offset ||
+    //   Math.abs(delta[1]) > this.offset
+    // ) {
+    //   this.pos = [...pos];
+    //   // this.pos[0] += Math.sign(delta[0]) * this.offset;
+    //   // this.pos[1] += Math.sign(delta[1]) * this.offset;
+    //   // this.pos[0] = pos[0]
+    //   // this.pos[0] += Math.sign(delta[0]) * this.offset;
+    // }
+    // if (
+    //   Math.abs(delta[1]) > this.offset
+    // ) {
+    //   // this.pos[1] = pos[1]
+    //   // this.pos[1] += Math.sign(delta[1]) * this.offset;
+    // }
     this.pos = [...pos];
+    // this.pos[0] += Math.sign(delta[0]) * this.offset;
+    // this.pos[1] += Math.sign(delta[1]) * this.offset;
     this.color = [...color];
     this.div.style.left = `${this.pos[0] - this.size / 2}px`;
     this.div.style.top = `${this.pos[1] - this.size / 2}px`;
@@ -163,33 +208,19 @@ class Box {
   };
 }
 
-const Bouncers = [];
+const Main = new BouncerContainer({
+  w: 800,
+  h: 600,
+  b: [
+    {
+      l: 5,
+      v: 50,
+      s: 50,
+      i: randomLocationOnScreen(800, 600, 20),
+      o: 10
+    },
+  ],
+  p: document.getElementById("bonka"),
+});
 
-function addBouncers(settings) {
-  for (let b = 0; b < settings.length; b++) {
-    const temp = settings[b];
-    Bouncers.push(new Bouncer(...temp));
-  }
-}
-function updateBouncers() {
-  Bouncers.forEach((element) => {
-    element.update();
-  });
-}
-function init(settings) {
-  addBouncers(settings);
-  setInterval(updateBouncers, 1000 / 60);
-}
-function randomLocationOnScreen(size) {
-  return [
-    Math.floor(Math.random() * (document.body.offsetWidth - size)),
-    Math.floor(Math.random() * (document.body.offsetHeight - size)),
-  ];
-}
-window.onload = init([
-  // [2, 10, 500, randomLocationOnScreen(500)],
-  [1, 10, 100, randomLocationOnScreen(50)],
-  [1, 10, 100, randomLocationOnScreen(50)],
-  [1, 10, 100, randomLocationOnScreen(50)],
-  [1, 10, 100, randomLocationOnScreen(50)],
-]);
+window.onload = Main.init();
